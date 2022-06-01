8319d777fd95abd60c89c0fcea5b409dc1d41af8
Erbol94 <erbolshakeev@gmail.com>
2022-06-01 16:22:47 +0600

    DZ6_Shakeev_Erbol_12-01

--- Four/lesson6/my-app/src/App.js
+++ Four/lesson6/my-app/src/App.js
@@ -5,6 +5,7 @@ import SecondPage from "./pages/secondPage/SecondPage";
 function App() {
   return (
     <div>
+        <h1>DZ6_Shakeev_Erbol_12-01</h1>
         <SecondPage />
         <MainPage />
     </div>
